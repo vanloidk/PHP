ac05c77cc1c952102ea3f7bb0df66da853653245
Nguyen Van Loi <49062878+vanloidk@users.noreply.github.com>
2019-10-11 01:27:49 +0700

    Update slacktest.php

--- export-slack-log/slacktest.php
+++ export-slack-log/slacktest.php
@@ -3,12 +3,10 @@
 /**
  * /slack_backup.php
  *
- * @copyright Copyright (C) 2015 X-TRANS inc.
  * @author Nguyen Van Loi
  * @package export_slack_log
  * @since Feb 18, 2016
  * @version $Id$
- * @license X-TRANS Develop License 1.0
  */
 
 /**
